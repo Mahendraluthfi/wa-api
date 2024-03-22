bb361d64d510eace4918d76f9dadee2b7cdbcf59
mahendraluthfi <mahendraluthfi@gmail.com>
2024-03-22 14:50:55 +0700

    fix 1000x

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors());
 
 app.use(express.json());
